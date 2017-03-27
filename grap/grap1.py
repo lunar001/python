b86fd65e4b9b37c9e0221e995c65003c1067ec67
Lunar002 <214607868@qq.com>
2017-03-27 08:04:06 -0400

    chapter 1 of book crawl

--- grap/grap1.py
+++ grap/grap1.py
@@ -6,13 +6,18 @@ import urllib2
 import urlparse
 import re
 import itertools
+import Throttle
 
-def download(url, user_agent = 'wswp', num_retries = 2):
+def download(url, user_agent = 'wswp', proxy = None, num_retries = 2):
     print 'Downloading:', url
     headers = {'User-agent': user_agent}
     request = urllib2.Request(url, headers = headers)
+    opener = urllib2.build_opener()
+    if proxy:
+        proxy_params = {urlparse.urlparse(url).schemme:proxy}
+        opener.add_handler(urllib2.ProxyHandler(proxy_params))
     try:
-        html = urllib2.urlopen(request).read()
+        html = opener.open(request).read()
     except urllib2.URLError as e:
         print 'Download error:', e.reason
         html = None
@@ -47,29 +52,30 @@ def crawl_idtree(url):
             num_errors = 0
             
             
-def link_crawler(seed_url, link_regex):
+def link_crawler(seed_url, link_regex, user_agent = 'wswp', max_depth = 1):
     crawl_queue= [seed_url]
-    seen = set(crawl_queue)
+    seen ={}
+    seen[seed_url] = 0 
+    throttle = Throttle.Throttle(10)
     while crawl_queue:
         url = crawl_queue.pop()
+        throttle.wait(url)
         html = download(url)
-        for link in get_links(html):
-            if re.match(link_regex, link):
-                print link
-                link = urlparse.urljoin(seed_url, link)
-                print link
+        depth = seen[url]
+        print depth
+        if depth != max_depth:
+            print get_links(html)
+            for link in get_links(html):
+                if re.match(link_regex, link):
+                    link = urlparse.urljoin(seed_url, link)
                 if link not in seen:
-                    seen.add(link)
+                    seen[link] = depth + 1
                     crawl_queue.append(link)
+
 def get_links(html):
     webpage_regex = re.compile('<a href="(.*?)"', re.IGNORECASE)
     return webpage_regex.findall(html)
 
-
-
-
 if __name__ == '__main__':
-    #test()
-    #crawl_idtree(sys.argv[1])
-    #crawl_sitemap(sys.argv[1])
+    print sys.argv
     link_crawler(sys.argv[1], sys.argv[2])
